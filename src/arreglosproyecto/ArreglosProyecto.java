b14205edee6b6df8713946fe00a7e447abc59a31
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-06-16 11:36:34 -0600

    Comenzando la elaboracion del codigo

--- src/arreglosproyecto/ArreglosProyecto.java
+++ src/arreglosproyecto/ArreglosProyecto.java
@@ -1,20 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package arreglosproyecto;
 
-/**
- *
- * @author Microsoft Windows 10
- */
+import java.util.Scanner;
+
 public class ArreglosProyecto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner teclado = new Scanner(System.in);
+
+        // Declaramos el arreglo que usaremos
+        String[] listaContactos = new String[10];
+        int contactosRegistrados = 0;
+
+        // Usamos un ciclo while que sea el menú de acciones y termine hasta que el usuario desea salir
+        int opcion = 0;
+        while (opcion != 6) {
+            System.out.println("Menu de Opciones: \n1. Agregar Contactos\n2. Ver Contactos\n3. Buscar Contacto por nombre\n4. Editar Contacto\n5. Eliminar Contacto\n6. Salir\nOpcion: ");
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println(" --- AGREGAR CONTACTOS --- ");
+                    System.out.println("Cuantos contactos deseas agregar?");
+                    int cantContactos = teclado.nextInt();
+
+                    if (cantContactos + contactosRegistrados > 12) {
+                        System.out.println("Limite de contactos sobrepasado. Limite - 12. Contactos Registrados: " + contactosRegistrados);
+                    } else {
+                        System.out.println("Escribe Nombre y Numero (EJEMPLO:Luis 4423567891)");
+                        for (int i = contactosRegistrados; i < cantContactos; i++) {
+                            String contacto = teclado.next();
+                            listaContactos[i] = contacto;
+
+                        }
+                        contactosRegistrados += cantContactos;
+                    }
+                    break;
+
+            }
+        }
     }
-    
+
 }
